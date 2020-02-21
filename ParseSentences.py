a676d53b6df044b2a8f1b8d26f5018f39e507b28
iambillmccann <bill.mccann@gmail.com>
2020-02-21 14:11:52 -0500

    Add docstring

--- ParseSentences.py
+++ ParseSentences.py
@@ -1,3 +1,16 @@
+""" This script is the first step of the data pipeline for analyzing "Interesting Words"
+
+This program will read files from the corpus, will break the content into sentences
+and will score the sentences for sentiment (positive or negative). The results are
+written to a file for processing by the next step.
+
+    Typical usage example:
+
+    $ python ParseSentences.py
+
+Note: It is presumed that the data files are in a folder named "corpus". The corpus
+folder is in the same folder as this script.
+"""
 from modules import utilities
 
 DATA_FOLDER = "./corpus"
@@ -5,7 +18,9 @@ DATA_FOLDER = "./corpus"
 def main():
 
     the_files = utilities.get_file_names(DATA_FOLDER)
-    print(DATA_FOLDER)
+    for file_name in the_files:
+        content = utilities.get_content(DATA_FOLDER, file_name)
+        print(content[:50])
 
 if __name__ == '__main__':
     main()
--- modules/utilities.py
+++ modules/utilities.py
@@ -1,5 +1,45 @@
+""" This script contains various helper functions.
+
+The functions in this script help keep the main scripts concise and readable.
+
+    Typical usage:
+
+    from modules import utilies
+"""
 from os import listdir
 from os.path import isfile, join
 
 def get_file_names(the_path):
+    """ Retrieve a list of file names for a folder
+
+    Use the listdir function to retrieve the objects in the folder,
+    then iterate over each using isfile to determine if the object
+    is a file or folder. Only files are returned.
+
+    Args:
+        the_path: the name of the folder to search
+
+    Returns:
+        a list containing the names of the files
+    """
     return [f for f in listdir(the_path) if isfile(join(the_path, f))]
+
+def get_content(the_path, the_file):
+    """ Retrieve the content of a file into a single string variable
+
+    Use the simple read() method to retrieve the contents of a file.
+    Note: This only works for small text files.
+
+    Args:
+        the_path: the folder containing the file
+        the_file: the name of the file
+
+    Returns:
+        the content of the file
+    """
+    full_path = '{}/{}'.format(the_path, the_file)
+    file_handle = open(full_path, "r")
+    content = file_handle.read()
+    file_handle.close()
+
+    return content
