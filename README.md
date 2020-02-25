5d3f2729fb93a6c1ff02774ca6a340d3244f5aca
iambillmccann <bill.mccann@gmail.com>
2020-02-25 17:53:49 -0500

    Test README markdown

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# InterestingWords
- This project will read text documents and perform an analysis on what are the interesting words.
+# Interesting Words
+### Coding Task
+
+**Bill McCann**
+25 February 2020
+bill.mccann@gmail.com
+
+This project will read text documents and perform an analysis on what are the interesting words.
+
+## Summary ##
+
+## Findings ##
+
+## 
--- runall.sh
+++ runall.sh
@@ -1,11 +1,22 @@
 #! /bin/bash -x
 echo Starting!
 
+echo Activate the Python virtual environment
 source ./venv/bin/activate
+
+echo Parse the raw data into sentences
 python ParseSentences.py
+
+echo Mark each sentence with sentiment scores
 python DetermineSentiment.py
+
+echo Tag each word with parts of speech
 python TagPartsOfSpeech.py
+
+echo Make the list of intersting words
 python InterestingWords.py
+
+echo Build the table for the final report
 python PrepareTable.py
 
 echo Finished!
